14148b491d560df887aeb2fb3146c2ee7f16885f
Arnaud Schwartz <gabz57@yahoo.fr>
2013-10-26 19:25:25 +0200

    Correction, j'ai oublié de switcher entre les branches de git avant de pusher la modif...

--- src/fr/utc/lo23/sharutc/SharUTC.java
+++ src/fr/utc/lo23/sharutc/SharUTC.java
@@ -11,18 +11,10 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
 /**
  *
  */
-public class SharUTC extends Application {
+public class SharUTC {
 
     private static final Logger log = LoggerFactory
             .getLogger(SharUTC.class);
@@ -49,27 +41,5 @@ public class SharUTC extends Application {
     }
 
     private static void startView(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 }
