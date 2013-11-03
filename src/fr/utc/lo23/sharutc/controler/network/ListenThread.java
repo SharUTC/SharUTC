154c04b9ba8c478859e0228ce1d97c61296cdc0e
Akim Sadaoui <mikayuoadas@gmail.com>
2013-11-03 02:40:06 +0100

    [Network] created base ListenThread class.

---
+++ src/fr/utc/lo23/sharutc/controler/network/ListenThread.java
@@ -0,0 +1,40 @@
+package fr.utc.lo23.sharutc.controler.network;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ */
+public class ListenThread implements Runnable {
+    private static final Logger log = LoggerFactory.getLogger(PeerDiscoverySocket.class);
+
+    /**
+     *
+     */
+    public ListenThread(int port, NetworkService networkService) {
+        log.warn("Not supported yet!");
+    }
+
+    /**
+     *
+     */
+    public void start() {
+        log.warn("Not supported yet!");
+    }
+
+    /**
+     *
+     */
+    public void stop() {
+        log.warn("Not supported yet!");
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void run() {
+        log.warn("Not supported yet!");
+    }
+}
