f46d3b655947c72e196dcc6996978ef22a08a0a7
AudreyBLo23 <Audrey.bramy@gmail.com>
2013-11-04 10:59:47 +0100

    Revert "[Network] NetworkServiceImpl : method implemented :"
    This reverts commit b20ef30a6afc7077f1841b88daba852ef0aa92b5.

--- src/fr/utc/lo23/sharutc/controler/network/NetworkServiceImpl.java
+++ src/fr/utc/lo23/sharutc/controler/network/NetworkServiceImpl.java
@@ -63,12 +63,7 @@ public class NetworkServiceImpl implements NetworkService {
      */
     @Override
     public void addPeer(long peerId, PeerSocket peerSocket) {
-        if(peerId == 0 || peerSocket == null){
-            log.error("[NetworkService - addPeer()] - null object");
-        } else {
-            this.mPeers.put(peerId, peerSocket);
-            log.info("[NetworkService - addPeer()] - peer "+peerId+" had been adding succesfully");
-        }
+        log.warn("addPeer - Not supported yet.");
     }
 
     /**
@@ -92,11 +87,7 @@ public class NetworkServiceImpl implements NetworkService {
      * {inheritDoc}
      */
     private void sendUnicast(Message message, Peer peer) {
-        if(peer != null){
-            log.error("[NetworkService - sendUnicast()] - object Peer is null");
-        } else {
-            this.mPeers.get(peer.getId()).send(message);
-        }
+        log.warn("sendUnicast - Not supported yet.");
     }
 
     /**
@@ -229,11 +220,7 @@ public class NetworkServiceImpl implements NetworkService {
      */
     @Override
     public void userInfoBroadcast(UserInfo userInfo) {
-        if(userInfo != null){
-            sendBroadcast(messageParser.write(MessageType.CONNECTION, new Object[][]{{Message.CONVERSATION_ID,appModel.getCurrentConversationId()},{Message.USER_INFO, userInfo}}));
-        } else {
-            log.equals("[NetworkService - userInfoBroadCast()] - userInfo is null");
-        }
+        log.warn("Not supported yet.");
     }
 
     /**
