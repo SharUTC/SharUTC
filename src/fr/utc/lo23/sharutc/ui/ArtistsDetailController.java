3dcf5c95225137cdfb6334bb5521adf59f76fa3c
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-11-05 16:12:12 +0100

    Create basic detail controllers

---
+++ src/fr/utc/lo23/sharutc/ui/ArtistsDetailController.java
@@ -0,0 +1,13 @@
+package fr.utc.lo23.sharutc.ui;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ArtistsDetailController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
