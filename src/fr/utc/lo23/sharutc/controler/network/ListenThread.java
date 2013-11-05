fecac975de6428129815d66b887c7c1deb1957e2
Arselle <Arselle@Arselle-PC>
2013-11-05 10:49:17 +0100

    Classe ListenThread

--- src/fr/utc/lo23/sharutc/controler/network/ListenThread.java
+++ src/fr/utc/lo23/sharutc/controler/network/ListenThread.java
@@ -2,32 +2,37 @@ package fr.utc.lo23.sharutc.controler.network;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import java.net.Socket;
+import java.io.IOException;
+import java.net.ServerSocket;
 /**
  *
  */
 public class ListenThread implements Runnable {
     private static final Logger log = LoggerFactory.getLogger(PeerDiscoverySocket.class);
-
+    private final int port;//final static int port;
+    private final NetworkService networkService;
+    
     /**
      *
      */
-    public ListenThread(int port, NetworkService networkService) {
-        log.warn("Not supported yet!");
+    public ListenThread(int p, NetworkService ns) {
+        this.port = p;
+        this.networkService = ns;
     }
 
     /**
      *
      */
     public void start() {
-        log.warn("Not supported yet!");
+        this.start();
     }
 
     /**
      *
      */
     public void stop() {
-        log.warn("Not supported yet!");
+        this.stop();
     }
 
     /**
@@ -35,6 +40,20 @@ public class ListenThread implements Runnable {
      */
     @Override
     public void run() {
-        log.warn("Not supported yet!");
+        long peerID = 20;   //valeur arbitraire pour l'instant A CHANGER
+        try {
+            ServerSocket socketServeur = new ServerSocket(port);
+            System.out.println("Lancement du serveur");
+            
+            while (socketServeur.isBound()) {
+                Socket socketClient = socketServeur.accept();
+                PeerSocket ps = new PeerSocket(socketClient,this.networkService,peerID);
+                networkService.addPeer(peerID, ps);
+                socketClient.close();
+            }
+            
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
     }
-}
+  }
