7699e14fa2c329beb287988b9043ca26f99e580f
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-11-06 11:16:14 +0100

    Add drop functionality

--- src/fr/utc/lo23/sharutc/ui/LoginController.java
+++ src/fr/utc/lo23/sharutc/ui/LoginController.java
@@ -1,5 +1,6 @@
 package fr.utc.lo23.sharutc.ui;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -7,11 +8,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.text.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,7 +30,6 @@ public class LoginController implements Initializable {
 
     private static final Logger log = LoggerFactory
             .getLogger(LoginController.class);
-
     public HBox titleContainter;
     public ImageView logoContainer;
     public Button buttonSignUp;
@@ -32,9 +37,13 @@ public class LoginController implements Initializable {
     public Button buttonImport;
     public TextField userNameField;
     public PasswordField passwordField;
+    //drop Overlay
+    public Region dropOverlay;
+    public Label dropOverlayLabel;
 
     /**
      * Initializes the controller class.
+     *
      * @param url
      * @param rb
      */
@@ -49,6 +58,9 @@ public class LoginController implements Initializable {
         logoContainer.setSmooth(true);
         logoContainer.setPreserveRatio(true);
         logoContainer.setImage(new Image("fr/utc/lo23/sharutc/ui/drawable/logo.png"));
+
+        //hide drop overlay
+        hideDropOverlay();
     }
 
     @FXML
@@ -67,5 +79,50 @@ public class LoginController implements Initializable {
         log.info("username: " + userNameField.getText() + " password: "
                 + passwordField.getText());
     }
+    
+    @FXML
+    public void handleDragEntered(DragEvent dragEvent) throws IOException {
+        showDropOverlay();
+        dragEvent.consume();
+    }
 
+    @FXML
+    public void handleDragOver(DragEvent dragEvent) throws IOException {
+        final Dragboard db = dragEvent.getDragboard();
+        if (db.hasFiles()) {
+            dragEvent.acceptTransferModes(TransferMode.ANY);
+        }        
+        dragEvent.consume();        
+    }
+
+    @FXML
+    public void handleDragExited(DragEvent dragEvent) throws IOException {
+        hideDropOverlay();
+        dragEvent.consume();
+    }
+
+    @FXML
+    public void handleDragDropped(DragEvent dragEvent) throws IOException {
+        final Dragboard db = dragEvent.getDragboard();
+        boolean success = false;
+        if (db.hasFiles()) {
+            success = true;
+            for (File file : db.getFiles()) {
+                log.info("file dropped, filePath: " + file.getAbsolutePath());
+            }
+        }
+        hideDropOverlay();
+        dragEvent.setDropCompleted(success);
+        dragEvent.consume();
+    }
+
+    private void hideDropOverlay() {
+        dropOverlay.setVisible(false);
+        dropOverlayLabel.setVisible(false);
+    }
+
+    private void showDropOverlay() {
+        dropOverlay.setVisible(true);
+        dropOverlayLabel.setVisible(true);
+    }
 }
