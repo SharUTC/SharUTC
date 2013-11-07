2b47b389079511f2948ba0e85c8a4c40de387621
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-11-07 10:01:12 +0100

    Use inheritance between Box

--- src/fr/utc/lo23/sharutc/ui/widget/SearchResultItemBox.java
+++ src/fr/utc/lo23/sharutc/ui/widget/SearchResultItemBox.java
@@ -5,14 +5,10 @@
  */
 package fr.utc.lo23.sharutc.ui.widget;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -23,29 +19,12 @@ public class SearchResultItemBox extends ItemBox {
     static final Logger log = Logger.getLogger(SearchResultItemBox.class.getName());
 
     protected String subTitle;
-    public Text boxtitle;
     public Text boxsubtitle;
 
     public SearchResultItemBox(String title, String subTitle) {
         super(title);
         this.subTitle = subTitle;
-
-
     }
-
-    public Pane buildPane() {
-        Pane pane = null;
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxml/search_result_item_box.fxml"));
-            loader.setController(this);
-            pane = (Pane) loader.load();
-
-        } catch (IOException ex) {
-            log.log(Level.SEVERE, null, ex);
-        }
-        return pane;
-    }
-
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
