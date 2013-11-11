5e65bd863ec1500e6965ad82e4f080347a3aa1ad
Florian Paillard <dev@sertook.com>
2013-11-11 22:11:38 +0100

    improvement of cardList

--- src/fr/utc/lo23/sharutc/ui/SearchResultController.java
+++ src/fr/utc/lo23/sharutc/ui/SearchResultController.java
@@ -1,7 +1,9 @@
 package fr.utc.lo23.sharutc.ui;
 
-import fr.utc.lo23.sharutc.ui.widget.SearchResultItemBox;
-import fr.utc.lo23.sharutc.ui.widget.SearchResultItemsList;
+import fr.utc.lo23.sharutc.model.userdata.UserInfo;
+import fr.utc.lo23.sharutc.ui.custom.PeopleCard;
+import fr.utc.lo23.sharutc.ui.custom.SimpleCard;
+import fr.utc.lo23.sharutc.ui.widget.CardList;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
@@ -11,6 +13,10 @@ import javafx.scene.layout.VBox;
 public class SearchResultController implements Initializable {
     public VBox gridpane;
     private String search;
+    private CardList songList;
+    private CardList friendList;
+    private CardList artistList;
+    private CardList albumList;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -20,18 +26,31 @@ public class SearchResultController implements Initializable {
             search = "";
         }
        
+        songList = new CardList("Songs");
+        friendList = new CardList("Friends");
+        artistList = new CardList("Artists");
+        albumList = new CardList("Albums");
         
-        SearchResultItemsList list = new SearchResultItemsList("album");
-         gridpane.getChildren().add(list.buildPane());
+        gridpane.getChildren().add(songList.buildPane());
+        gridpane.getChildren().add(friendList.buildPane());
+        gridpane.getChildren().add(artistList.buildPane());
+        gridpane.getChildren().add(albumList.buildPane());
         
-        list.addChild(new SearchResultItemBox("artiste1", "soustitre 1"));
-        list.addChild(new SearchResultItemBox("artiste2", "soustitre 2"));
-        
-          list = new SearchResultItemsList("album");
-         gridpane.getChildren().add(list.buildPane());
-        
-        list.addChild(new SearchResultItemBox("artiste1", "soustitre 1"));
-        list.addChild(new SearchResultItemBox("artiste2", "soustitre 2"));
-        
+        UserInfo u = new UserInfo();
+        u.setFirstName("bob");
+        addChild(new PeopleCard(u));
+        addChild(new PeopleCard(u));
+        addChild(new PeopleCard(u));
+        addChild(new PeopleCard(u));
+        addChild(new PeopleCard(u));
+        addChild(new PeopleCard(u));
+       
+    }
+    
+    public void addChild(SimpleCard card){
+        if(card instanceof PeopleCard){
+            friendList.addChild(card);
+            
+        }//TODO else if(card instanceof )
     }
 }
--- src/fr/utc/lo23/sharutc/ui/widget/CardList.java
+++ src/fr/utc/lo23/sharutc/ui/widget/CardList.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package fr.utc.lo23.sharutc.ui.widget;
 
+import fr.utc.lo23.sharutc.ui.custom.SimpleCard;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.FlowPane;
@@ -13,25 +13,37 @@ import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
-public class ItemsList implements Initializable {
+public class CardList implements Initializable {
 
-    static final Logger log = Logger.getLogger(SearchResultItemBox.class.getName());
+    static final Logger log = Logger.getLogger(CardList.class.getName());
 
+    private final static int MIN_CARD = 4;
+    private boolean showAll = false;
+
+    private String title;
+    public Text boxtitle;
     public FlowPane content;
+    public Button morebt;
+    private List<SimpleCard> childs = new ArrayList<SimpleCard>();
 
-
-    public ItemsList() {
-
+    public CardList(String title) {
+        this.title = title;
     }
 
     public Pane buildPane() {
         Pane pane = null;
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxml/items_list.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxml/card_list.fxml"));
             loader.setController(this);
             pane = (Pane) loader.load();
 
@@ -41,13 +53,43 @@ public class ItemsList implements Initializable {
         return pane;
     }
 
-
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        boxtitle.setText(title);
+
     }
 
-    public void addChild(ItemBox item) {
-        content.getChildren().add(item.buildPane("../fxml/item_box.fxml"));
+    @FXML
+    private void handleMenuButtonAction(ActionEvent event) throws IOException {
+        if (event.getSource() == morebt) {
+            showAll = !showAll;
+            refreshListVisibility();
+        }
+    }
+
+    public void addChild(SimpleCard card) {
+        childs.add(card);
+
+        if (showAll || content.getChildren().size() < MIN_CARD) {
+            content.getChildren().add(card);
+        }
+
+    }
+
+    private void refreshListVisibility() {
+        if (showAll) {
+            for (int i = content.getChildren().size(); i < childs.size(); i++) {
+
+                content.getChildren().add(childs.get(i));
+
+            }
+        } else {
+            for (int i = content.getChildren().size() - 1; i >= MIN_CARD; i--) {
+
+                content.getChildren().remove(i);
+            }
+
+        }
 
     }
 
