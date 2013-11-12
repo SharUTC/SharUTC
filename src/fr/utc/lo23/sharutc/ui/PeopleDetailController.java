84a10e65cf7833ddbcdf3d8efe2bcd032e6f52f7
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-11-12 14:54:13 +0100

    Handle add to friends click

--- src/fr/utc/lo23/sharutc/ui/PeopleDetailController.java
+++ src/fr/utc/lo23/sharutc/ui/PeopleDetailController.java
@@ -1,7 +1,9 @@
 package fr.utc.lo23.sharutc.ui;
 
 import fr.utc.lo23.sharutc.model.userdata.UserInfo;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.net.URL;
@@ -10,6 +12,7 @@ import java.util.ResourceBundle;
 public class PeopleDetailController implements Initializable {
 
     public Label login;
+    public Button addToFriendsButton;
     private UserInfo mUserInfo;
 
     @Override
@@ -20,5 +23,9 @@ public class PeopleDetailController implements Initializable {
     public void setUserInfo(UserInfo userInfo) {
         mUserInfo = userInfo;
         login.setText(mUserInfo.getLogin());
+    }
+
+    public void handleAddToFriendsClicked(ActionEvent actionEvent) {
+
     }
 }
