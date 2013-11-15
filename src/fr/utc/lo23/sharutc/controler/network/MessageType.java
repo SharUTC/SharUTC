f3591f40943b97d5d4cb121517ca6b9a24fe71d8
AudreyBLo23 <Audrey.bramy@gmail.com>
2013-11-15 16:21:33 +0100

    [Network] added Javadoc

--- src/fr/utc/lo23/sharutc/controler/network/MessageType.java
+++ src/fr/utc/lo23/sharutc/controler/network/MessageType.java
@@ -6,83 +6,83 @@ package fr.utc.lo23.sharutc.controler.network;
 public enum MessageType {
 
     /**
-     *
+     * Send message to get a catalog from a peer
      */
     MUSIC_GET_CATALOG,
     /**
-     *
+     * Send message with the own catalog
      */
     MUSIC_CATALOG,
     /**
-     * => sendTagMapCommand
+     * Send message to get tag map from a peer
      */
     TAG_GET_MAP,
     /**
-     * => integrateRemoteTagMapCommand
+     * Send message containing own tag map
      */
     TAG_MAP,
     /**
-     *
+     * Add a comment on a music
      */
     COMMENT_ADD,
     /**
-     *
+     * Edit a music comment
      */
     COMMENT_EDIT,
     /**
-     *
+     * Remove a music comment
      */
     COMMENT_REMOVE,
     /**
-     *
+     * Set a music score
      */
     SCORE_SET,
     /**
-     *
+     * Unset a music score
      */
     SCORE_UNSET,
     /**
-     *
+     * Send a message to search a music
      */
     MUSIC_SEARCH,
     /**
-     *
+     * Send a message return a list of music corresponding to a search
      */
     MUSIC_RESULTS,
     /**
-     *
+     * Send message to get a music
      */
     MUSIC_GET,
     /**
-     *
+     * Put a music in the local library
      */
     MUSIC_INSTALL,
     /**
-     *
+     * Get a music to play
      */
     MUSIC_GET_TO_PLAY,
     /**
-     *
+     * Send a music to a peer
      */
     MUSIC_SEND_TO_PLAY,
     /**
-     *
+     * Send a disconnection message to the network 
      */
     DISCONNECT,
     /**
-     *
+     * Send a connection message to the network 
      */
     CONNECTION,
     /**
-     *
+     * Send a connection response to a peer just connected
      */
     CONNECTION_RESPONSE,
     /**
-     * 
+     * Send info of a peer just connected
      */
     USER_INFO,
     /**
-     * 
+     * Edit a music comments
      */
     EDIT_COMMENT
 }
