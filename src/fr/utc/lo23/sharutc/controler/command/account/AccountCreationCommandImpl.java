7f96db93ce254ad82bfaf1715763a60ebea392e8
DeSweetChebs <DeSweetChebs@DeSweetChebs-PC>
2013-11-20 00:38:02 +0100

    Check if a profile with the same login already exists on the computer.

--- src/fr/utc/lo23/sharutc/controler/command/account/AccountCreationCommandImpl.java
+++ src/fr/utc/lo23/sharutc/controler/command/account/AccountCreationCommandImpl.java
@@ -4,7 +4,10 @@ import com.google.inject.Inject;
 import fr.utc.lo23.sharutc.controler.service.FileService;
 import fr.utc.lo23.sharutc.controler.service.MusicService;
 import fr.utc.lo23.sharutc.controler.service.UserService;
+import fr.utc.lo23.sharutc.model.AppModel;
+import fr.utc.lo23.sharutc.model.ErrorMessage;
 import fr.utc.lo23.sharutc.model.userdata.UserInfo;
+import java.io.File;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,13 +18,15 @@ public class AccountCreationCommandImpl implements AccountCreationCommand {
 
     private static final Logger log = LoggerFactory
             .getLogger(AccountCreationCommandImpl.class);
+    private AppModel appModel;
     private UserInfo mUserInfo;
     private final UserService userService;
     private final MusicService musicService;
     private final FileService fileService;
 
     @Inject
-    public AccountCreationCommandImpl(UserService userService, FileService fileService, MusicService musicService) {
+    public AccountCreationCommandImpl(AppModel appModel, UserService userService, FileService fileService, MusicService musicService) {
+        this.appModel = appModel;
         this.userService = userService;
         this.fileService = fileService;
         this.musicService = musicService;
@@ -49,22 +54,32 @@ public class AccountCreationCommandImpl implements AccountCreationCommand {
     @Override
     public void execute() {
         log.info("AccountCreationCommand ...");
+        StringBuilder builder = new StringBuilder(fileService.getAppFolder()).append(FileService.ROOT_FOLDER_USERS).append(File.separator).append(mUserInfo.getLogin());
+        File file = new File(builder.toString());
+        
+        if(file.exists() == false){
+            // FIXME userProfile validation ??
+            // it should be performed by the command or a service,
+            // not in ui, use ErrorMessage and ErrorBus
 
-        // FIXME userProfile validation ??
-        // it should be performed by the command or a service,
-        // not in ui, use ErrorMessage and ErrorBus
+            musicService.createAndSetCatalog();
+            musicService.createAndSetRightsList();
+            // let userService.createAndSetProfile(mUserInfo) call AFTER
+            // musicService calls, we only inform once the ui that the user has
+            // changed by setting profile attribute in appModel
+            userService.createAndSetProfile(mUserInfo);
 
-        musicService.createAndSetCatalog();
-        musicService.createAndSetRightsList();
-        // let userService.createAndSetProfile(mUserInfo) call AFTER
-        // musicService calls, we only inform once the ui that the user has
-        // changed by setting profile attribute in appModel
-        userService.createAndSetProfile(mUserInfo);
+            fileService.createAccountFolder(mUserInfo.getLogin());
+            userService.saveProfileFiles();
+            musicService.saveUserMusicFile();
+            musicService.saveUserRightsListFile();
+            log.info("AccountCreationCommand DONE");
+            }
+        else{
+            log.warn("Can't create the profile. A profile with the same login already exists.");
+            ErrorMessage nErrorMessage = new ErrorMessage("Can't create the profile. A profile with the same login already exists.");
+            appModel.getErrorBus().pushErrorMessage(nErrorMessage);
+        }
 
-        fileService.createAccountFolder(mUserInfo.getLogin());
-        userService.saveProfileFiles();
-        musicService.saveUserMusicFile();
-        musicService.saveUserRightsListFile();
-        log.info("AccountCreationCommand DONE");
     }
 }
