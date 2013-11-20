04f298781ae4b7c8778623f92216560303e25c7b
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-11-20 14:21:47 +0100

    Use AddToLocalCatalogCommand

--- src/fr/utc/lo23/sharutc/ui/SongListController.java
+++ src/fr/utc/lo23/sharutc/ui/SongListController.java
@@ -1,5 +1,7 @@
 package fr.utc.lo23.sharutc.ui;
 
+import com.google.inject.Inject;
+import fr.utc.lo23.sharutc.controler.command.music.AddToLocalCatalogCommand;
 import fr.utc.lo23.sharutc.model.domain.Music;
 import fr.utc.lo23.sharutc.ui.custom.card.SongCard;
 import fr.utc.lo23.sharutc.ui.custom.card.TagCard;
@@ -16,6 +18,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.geometry.Insets;
 import javafx.scene.control.ScrollPane;
@@ -33,6 +36,8 @@ public class SongListController extends SongSelectorController implements Initia
     public HBox tagContainer;
     @FXML
     public ScrollPane tagScrollPane;
+    @Inject
+    private AddToLocalCatalogCommand mAddToLocalCatalogCommand;
 
     @Override
     public void init(StackPane dragPreview) {
@@ -42,7 +47,7 @@ public class SongListController extends SongSelectorController implements Initia
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        
+
         tagScrollPane.getStyleClass().add("myScrollPaneWithTopBorder");
 
         addNewSongButton.setOnAction(new EventHandler<ActionEvent>() {
@@ -53,27 +58,28 @@ public class SongListController extends SongSelectorController implements Initia
                 final FileChooser fileChooser = new FileChooser();
                 fileChooser.setTitle("Add a Song");
                 fileChooser.getExtensionFilters().add(extFilter);
-                final File file = fileChooser.showOpenDialog(addNewSongButton.getScene().getWindow());
-                if (file != null) {
-                    log.info("import song, filePath: " + file.getAbsolutePath());
-                    songAdded(file);
+                final List<File> files = fileChooser.showOpenMultipleDialog(addNewSongButton.getScene().getWindow());
+                if (files.size() > 0) {
+                    log.info("import songs !");
+                    mAddToLocalCatalogCommand.setFiles(files);
+                    mAddToLocalCatalogCommand.execute();
                 }
             }
         });
-        
+
         showTags();
 
     }
-    
+
     public void createCards(String artistName, String albumName) {
-        for(Music m : MainController.population) {
-            if((m.getArtist().equals(artistName) && m.getAlbum().equals(albumName)) || (artistName.equals("") && albumName.equals(""))) {
+        for (Music m : MainController.population) {
+            if ((m.getArtist().equals(artistName) && m.getAlbum().equals(albumName)) || (artistName.equals("") && albumName.equals(""))) {
                 SongCard newCard = new SongCard(m, this, true);
                 songsContainer.getChildren().add(newCard);
             }
         }
     }
-    
+
     public void createCards() {
         createCards("", "");
     }
@@ -89,22 +95,22 @@ public class SongListController extends SongSelectorController implements Initia
         songsContainer.getChildren().add(new SongCard(m, this, true));
         //TODO add song to user songs
     }
-    
+
     private void showTags() {
         tagContainer.getChildren().clear();
-        
+
         //The "virtual" "All songs" tag
         showTagCard(new TagCard("All Songs"));
-        
+
         //The "virtual" "My Songs" tag
         showTagCard(new TagCard("My Songs"));
-        
+
         //TODO remove when we get the real tags
-        for(int i=0; i< 5; i ++) {
+        for (int i = 0; i < 5; i++) {
             showTagCard(new TagCard("Tag " + String.valueOf(i)));
         }
     }
-    
+
     private void showTagCard(TagCard tagCard) {
         HBox.setMargin(tagCard, new Insets(0, 5, 0, 5));
         tagContainer.getChildren().add(tagCard);
