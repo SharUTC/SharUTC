6e91a5fae53d3dd2f8fc0ffe16bec018176e8c8d
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-11-20 11:12:04 +0100

    Add CommentView and use it in song detail

--- src/fr/utc/lo23/sharutc/ui/SongDetailController.java
+++ src/fr/utc/lo23/sharutc/ui/SongDetailController.java
@@ -1,6 +1,8 @@
 package fr.utc.lo23.sharutc.ui;
 
+import fr.utc.lo23.sharutc.model.domain.Comment;
 import fr.utc.lo23.sharutc.model.domain.Music;
+import fr.utc.lo23.sharutc.ui.custom.CommentView;
 import fr.utc.lo23.sharutc.ui.custom.card.DraggableCard;
 import fr.utc.lo23.sharutc.ui.custom.card.SongCard;
 import java.net.URL;
@@ -14,19 +16,48 @@ public class SongDetailController implements Initializable, SongCard.ISongCard {
 
     @FXML
     public VBox topLeftContainer;
-    
+    @FXML
+    public VBox commentContainer;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
         final Music music = new Music();
         music.setAlbum("Natural Mystic");
         music.setTitle("Crazy Baldhead");
         music.setArtist("Bob Marley");
-        
-        final SongCard songCard = new SongCard(music , this, false);
+
+        final SongCard songCard = new SongCard(music, this, false);
         songCard.setPrefWidth(230);
         topLeftContainer.getChildren().add(songCard);
-    }    
+        
+        //artificialy populate with some comments
+        populateCommentContainer();
+    }
+
+    private void populateCommentContainer() {
+        final Comment comment1 = new Comment();
+        comment1.setAuthorName("Jake");
+        comment1.setIndex(1);
+        comment1.setText("This song saved my life");
+
+        final Comment comment2 = new Comment();
+        comment2.setAuthorName("Amelia");
+        comment2.setIndex(2);
+        comment2.setText("Sick !");
+
+
+        final Comment comment3 = new Comment();
+        comment3.setAuthorName("PainInTheNeck");
+        comment3.setIndex(3);
+        comment3.setText("I'm not realy a fan of the genre. Is that event music ?!?");
+
+        commentContainer.getChildren().add(new CommentView(comment1));
+        commentContainer.getChildren().add(new CommentView(comment2));
+        commentContainer.getChildren().add(new CommentView(comment3));
+        commentContainer.getChildren().add(new CommentView(comment1));
+
+    }
 
     @Override
     public void onPlayRequested(Music music) {
@@ -62,5 +93,4 @@ public class SongDetailController implements Initializable, SongCard.ISongCard {
     public void onDragStop(DraggableCard draggableCard) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
 }
