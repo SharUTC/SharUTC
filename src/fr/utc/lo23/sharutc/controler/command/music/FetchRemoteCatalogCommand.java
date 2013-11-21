a3a335c3a01b99d0570268176d9eb4ae358e423e
Cécile <selphira.neko@gmail.com>
2013-11-21 15:55:39 +0100

    Added Javadoc to FetchRemoteCatalogCommand, RemoveFromLocalCatalogCommand, and SendCatalogCommand

--- src/fr/utc/lo23/sharutc/controler/command/music/FetchRemoteCatalogCommand.java
+++ src/fr/utc/lo23/sharutc/controler/command/music/FetchRemoteCatalogCommand.java
@@ -4,18 +4,22 @@ import fr.utc.lo23.sharutc.controler.command.Command;
 import fr.utc.lo23.sharutc.model.userdata.Peer;
 
 /**
- *
+ * Command for asking for a peer's catalog (+ clears current result catalog)
  */
 public interface FetchRemoteCatalogCommand extends Command {
 
     /**
-     *
+     * 
+     * Gets the peer who will be asked for his catalog when the command is executed
+     * 
      * @return
      */
     public Peer getPeer();
 
     /**
      *
+     * Sets the peer who will be asked for his catalog when the command is executed
+     * 
      * @param peer
      */
     public void setPeer(Peer peer);
