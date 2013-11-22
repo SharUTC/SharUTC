f28deb60481ab3d7a90a93497be8da6cf3093d2a
Akim Sadaoui <mikayuoadas@gmail.com>
2013-11-22 02:14:33 +0100

    [Tests] Added test for sendMusicCommand, playMusicCommand and sendMusicToPlayCommand.

---
+++ test/tests/NetworkMusicDownloadTestModule.java
@@ -0,0 +1,14 @@
+package tests;
+
+/**
+ *
+ */
+public class NetworkMusicDownloadTestModule extends TestModule{
+    /**
+     *
+     */
+    @Override
+    public void configure(){
+        super.configure();
+    }
+}
