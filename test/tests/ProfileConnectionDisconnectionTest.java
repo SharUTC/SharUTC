c4048f265ca554cee38368b0181e761a7141c95c
Tudor Luchiancenco <tudorluchy@gmail.com>
2013-11-23 11:35:33 +0100

    Tests : ProfileConnectionDisconnectionTest update
    ConnectionDisconnectionTest removed

--- test/tests/ProfileConnectionDisconnectionTest.java
+++ test/tests/ProfileConnectionDisconnectionTest.java
@@ -2,12 +2,18 @@ package tests;
 
 import com.google.inject.Inject;
 import fr.utc.lo23.sharutc.GuiceJUnitRunner;
+import fr.utc.lo23.sharutc.controler.command.account.AccountCreationCommand;
 import fr.utc.lo23.sharutc.controler.command.account.ConnectionRequestCommand;
 import fr.utc.lo23.sharutc.controler.command.account.DisconnectionCommand;
+import fr.utc.lo23.sharutc.controler.network.Message;
+import fr.utc.lo23.sharutc.controler.network.MessageParser;
+import fr.utc.lo23.sharutc.controler.network.MessageType;
+import fr.utc.lo23.sharutc.controler.network.NetworkServiceMock;
 import fr.utc.lo23.sharutc.controler.service.MusicService;
 import fr.utc.lo23.sharutc.controler.service.UserService;
 import fr.utc.lo23.sharutc.model.AppModel;
 import fr.utc.lo23.sharutc.model.AppModelBuilder;
+import fr.utc.lo23.sharutc.model.userdata.UserInfo;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -18,14 +24,15 @@ import org.slf4j.LoggerFactory;
 
 /**
  *
- * @author Olivier
+ *
+ *
  */
 @RunWith(GuiceJUnitRunner.class)
-@GuiceJUnitRunner.GuiceModules({ProfileConnectionDisconnectionTestModule.class})
+@GuiceJUnitRunner.GuiceModules({NetworkConnectionDisconnectionTestModule.class})
 public class ProfileConnectionDisconnectionTest {
 
     private static final Logger log = LoggerFactory
-            .getLogger(ProfileConnectionDisconnectionTest.class);
+        .getLogger(ProfileConnectionDisconnectionTest.class);
     @Inject
     private AppModel appModel;
     @Inject
@@ -33,11 +40,20 @@ public class ProfileConnectionDisconnectionTest {
     @Inject
     private MusicService musicService;
     @Inject
+    private NetworkServiceMock networkService;
+    @Inject
     private ConnectionRequestCommand connectionRequestCommand;
     @Inject
     private DisconnectionCommand disconnectionCommand;
+    @Inject
+    private AccountCreationCommand accountCreationCommand;
+    @Inject
+    private MessageParser messageParser;
     private AppModelBuilder appModelBuilder = null;
 
+    /**
+     *
+     */
     @Before
     public void before() {
         log.trace("building appModel");
@@ -47,22 +63,69 @@ public class ProfileConnectionDisconnectionTest {
         appModelBuilder.mockAppModel();
     }
 
+    /**
+     *
+     */
     @After
     public void after() {
         log.trace("cleaning appModel");
         appModelBuilder.clearAppModel();
+        appModelBuilder.deleteFolders();
     }
 
+    /**
+     *
+     */
     @Test
     public void connectionRequestCommand() {
-        //TODO test
-        Assert.assertTrue(false);
+        // add first user
+        String login = "tudorluchy1";
+        String password = "password1";
+        Long peerId = 4L;
+
+        UserInfo userInfo = new UserInfo();
+        userInfo.setLogin(login);
+        userInfo.setPassword(password);
+        userInfo.setPeerId(peerId);
+        userInfo.setFirstName("Tudor");
+        userInfo.setLastName("Luchiancenco");
+        userInfo.setAge(22);
+
+        // create account
+        accountCreationCommand.setUserInfo(userInfo);
+        accountCreationCommand.execute();
+
+        // call command
+        connectionRequestCommand.setLogin("tudorluchy1");
+        connectionRequestCommand.setPassword("password1");
+        connectionRequestCommand.execute();
+
+        // tests
+        Message m = networkService.getSentMessage();
+        Assert.assertNotNull("Message null", m);
+
+        messageParser.read(m);
+        UserInfo receivedUserInfo = (UserInfo) messageParser.getValue(Message.USER_INFO);
+        Assert.assertEquals("The mesage type must be : CONNECTION", MessageType.CONNECTION, m.getType());
+        Assert.assertEquals("The login don't match", login, receivedUserInfo.getLogin());
+        Assert.assertEquals("The password don't match", password, receivedUserInfo.getPassword());
+        Assert.assertEquals("The peerId don't match", peerId, receivedUserInfo.getPeerId());
+
     }
 
+    /**
+     *
+     */
     @Test
     public void disconnectionCommand() {
-        //Call disconnection command
         disconnectionCommand.execute();
+
+        // tests
+        Message m = networkService.getSentMessage();
+        Assert.assertNotNull("Message null", m);
+
+        messageParser.read(m);
+        Assert.assertEquals("The mesage type must be : DISCONNECT", MessageType.DISCONNECT, m.getType());
 
         Assert.assertNull(appModel.getProfile());
         Assert.assertNull(appModel.getLocalCatalog());
@@ -72,8 +135,5 @@ public class ProfileConnectionDisconnectionTest {
         Assert.assertEquals("RemoteUserCatalog not empty", 0, appModel.getRemoteUserCatalog().size());
         Assert.assertEquals("SearchResults not empty", 0, appModel.getSearchResults().size());
         Assert.assertEquals("TmpCatalog not empty", 0, appModel.getTmpCatalog().size());
-
-        Assert.assertTrue(false);// disconnectionCommand is not finished, 
-        // TODO check content with code (list size, id values, not null and null values)
     }
 }
