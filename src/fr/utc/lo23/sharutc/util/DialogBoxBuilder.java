bbf3b62454a35f8c2b5d4499f8e2d17941e44e2c
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-12-02 22:10:36 +0100

    Add ConfirmDialog for futur usage

--- src/fr/utc/lo23/sharutc/util/DialogBoxBuilder.java
+++ src/fr/utc/lo23/sharutc/util/DialogBoxBuilder.java
@@ -37,7 +37,7 @@ public class DialogBoxBuilder {
                                         LabelBuilder.create().text(label).build(),
                                         editableName)
                                         .build(),
-                                ButtonBuilder.create().styleClass("button").text("Validate").defaultButton(true).onAction(new EventHandler<ActionEvent>() {
+                                ButtonBuilder.create().styleClass("positiveButton").text("Validate").defaultButton(true).onAction(new EventHandler<ActionEvent>() {
                                     @Override
                                     public void handle(ActionEvent actionEvent) {
                                         // take action and close the dialog.
@@ -53,9 +53,57 @@ public class DialogBoxBuilder {
     }
 
     /**
+     * Create a Confirm box : yes or no
+     *
+     * @param label    instruction displayed to the user
+     * @param style    stylesheet class
+     * @param callback catch user choice
+     * @return Stage to show
+     */
+    public static Stage createConfirmBox(final String label, final String style, final IConfirmBox callback) {
+        final Stage dialog = new Stage(StageStyle.TRANSPARENT);
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.setScene(
+                new Scene(
+                        VBoxBuilder.create().styleClass("modal-dialog").children(
+                                LabelBuilder.create().text(label).build(),
+                                HBoxBuilder.create().styleClass("modal-dialog").children(
+                                        ButtonBuilder.create().styleClass("positiveButton").text("Yes").defaultButton(true).onAction(new EventHandler<ActionEvent>() {
+                                            @Override
+                                            public void handle(ActionEvent actionEvent) {
+                                                callback.onChoiceMade(true);
+                                                dialog.close();
+                                            }
+                                        }
+                                        ).build(),
+                                        ButtonBuilder.create().styleClass("negativeButton").text("No ").defaultButton(true).onAction(new EventHandler<ActionEvent>() {
+                                            @Override
+                                            public void handle(ActionEvent actionEvent) {
+                                                callback.onChoiceMade(false);
+                                                dialog.close();
+                                            }
+                                        }
+                                        ).build()
+                                ).build()
+
+                        ).build()
+                )
+        );
+        dialog.getScene().getStylesheets().add(style);
+        return dialog;
+    }
+
+    /**
      * Get callback from an EditDialogBox
      */
     public interface IEditBox {
         public void onValidate(String value);
+    }
+
+    /**
+     * Get answer from a ConfirmDialogBox
+     */
+    public interface IConfirmBox {
+        public void onChoiceMade(boolean answer);
     }
 }
