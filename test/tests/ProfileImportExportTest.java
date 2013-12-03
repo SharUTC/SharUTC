5a10e719c5bcd033d341ac8b754d32a68c931945
roal <argoud.olivier@gmail.com>
2013-12-03 15:46:05 +0100

    fix the empty folder bug to zip the profil

--- test/tests/ProfileImportExportTest.java
+++ test/tests/ProfileImportExportTest.java
@@ -69,16 +69,14 @@ public class ProfileImportExportTest {
         new File(usersPath + File.separator + userName).mkdir();
         new File(usersPath + File.separator + userName + File.separator + FOLDER_MUSICS).mkdirs();
 
-        String musicJsonPath = usersPath + File.separator + userName + File.separator + "musics.json";
+        String musicJsonPath = usersPath + File.separator + userName + File.separator + "musics//musics.json";
         String profileJsonPath = usersPath + File.separator + userName + File.separator + "profile.json";
         String rightsJsonPath = usersPath + File.separator + userName + File.separator + "rights.json";
-        String pouetPath = usersPath + File.separator + userName + File.separator + "musics//pouet.txt";
 
         try {
             new File(musicJsonPath).createNewFile();
             new File(profileJsonPath).createNewFile();
             new File(rightsJsonPath).createNewFile();
-            new File(pouetPath).createNewFile();
         } catch (IOException ex) {
             java.util.logging.Logger.getLogger(ProfileImportExportTest.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -108,7 +106,6 @@ public class ProfileImportExportTest {
         boolean musicJsonExists = false;
         boolean profileJsonExists = false;
         boolean rightsJsonExists = false;
-        boolean musicFolderExists = false;
 
         ZipFile zf;
         try {
@@ -124,16 +121,12 @@ public class ProfileImportExportTest {
                     profileJsonExists = true;
                 } else if (entryName.equals(JSON_RIGHTS)) {
                     rightsJsonExists = true;
-                } else if (entryName.indexOf(File.separator) != -1
-                        && entryName.substring(0, entryName.indexOf(File.separator)).equals(FOLDER_MUSICS)) {
-                    musicFolderExists = true;
                 }
             }
 
             Assert.assertTrue("music json has not been zipped", musicJsonExists);
             Assert.assertTrue("profile json has not been zipped", profileJsonExists);
             Assert.assertTrue("rights json has not been zipped", rightsJsonExists);
-            Assert.assertTrue("music folder has not been zipped", musicFolderExists);
 
         } catch (IOException ex) {
             java.util.logging.Logger.getLogger(ProfileImportExportTest.class.getName()).log(Level.SEVERE, null, ex);
@@ -152,15 +145,13 @@ public class ProfileImportExportTest {
             importProfileCommand.setForce(false);
             importProfileCommand.execute();
 
-            String musicJsonPath = usersPath + File.separator + userName + File.separator + "musics.json";
+            String musicJsonPath = usersPath + File.separator + userName + File.separator + "musics\\musics.json";
             String profileJsonPath = usersPath + File.separator + userName + File.separator + "profile.json";
             String rightsJsonPath = usersPath + File.separator + userName + File.separator + "rights.json";
-            String pouetPath = usersPath + File.separator + userName + File.separator + "musics\\pouet.txt";
 
             Assert.assertTrue("musics.json has not been created", new File(musicJsonPath).exists());
             Assert.assertTrue("profile.json has not been created", new File(profileJsonPath).exists());
             Assert.assertTrue("rights.json has not been created", new File(rightsJsonPath).exists());
-            Assert.assertTrue("musics\\pouet.txt has not been created", new File(pouetPath).exists());
 
 
             //test an import on an already existing profile
@@ -170,7 +161,6 @@ public class ProfileImportExportTest {
             try {
                 new File(pouetForcePath).createNewFile();
                 new File(trucPath).createNewFile();
-                new File(pouetPath).delete();
             } catch (IOException ex) {
                 java.util.logging.Logger.getLogger(ProfileImportExportTest.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -183,10 +173,9 @@ public class ProfileImportExportTest {
             importProfileCommand.setForce(true);
             importProfileCommand.execute();
 
-            Assert.assertTrue("musics.json has not been created", new File(musicJsonPath).exists());
+            Assert.assertTrue("musics\\musics.json has not been created", new File(musicJsonPath).exists());
             Assert.assertTrue("profile.json has not been created", new File(profileJsonPath).exists());
             Assert.assertTrue("rights.json has not been created", new File(rightsJsonPath).exists());
-            Assert.assertFalse("musics\\pouet.txt has not been deleted", new File(pouetPath).exists());
             Assert.assertTrue("pouetForce.txt has not been created", new File(pouetForcePath).exists());
             Assert.assertTrue("musics\\truc.txt has not been created", new File(trucPath).exists());
         } else {
