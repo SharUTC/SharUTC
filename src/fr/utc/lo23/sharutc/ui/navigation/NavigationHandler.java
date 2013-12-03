e53f8cfdcd4cde3569ed1f737e9485bd70e528b6
Arnaud Schwartz <gabz57@yahoo.fr>
2013-12-03 11:19:09 +0100

    Fixed my own mistake, pulled accidentaly branch ui to master

--- src/fr/utc/lo23/sharutc/ui/navigation/NavigationHandler.java
+++
@@ -1,7 +0,0 @@
-package fr.utc.lo23.sharutc.ui.navigation;
-
-public interface NavigationHandler {    
-   public void goToLoginPage();
-   public void goToRegistrationPage();
-   public void goToMainPage();
-}
