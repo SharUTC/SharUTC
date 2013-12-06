1785bb54f376a38fabe3fc1489eb71c0f1144070
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-12-06 16:40:51 +0100

    Add RightGroupBar

--- src/fr/utc/lo23/sharutc/ui/GroupRightController.java
+++ src/fr/utc/lo23/sharutc/ui/GroupRightController.java
@@ -7,100 +7,144 @@
 package fr.utc.lo23.sharutc.ui;
 
 import com.google.inject.Inject;
+import fr.utc.lo23.sharutc.controler.command.profile.ManageRightsCommand;
 import fr.utc.lo23.sharutc.model.domain.Music;
-import fr.utc.lo23.sharutc.controler.command.profile.*;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import fr.utc.lo23.sharutc.model.userdata.Category;
+import fr.utc.lo23.sharutc.ui.custom.HorizontalScrollHandler;
+import fr.utc.lo23.sharutc.ui.custom.card.RightCard;
+import fr.utc.lo23.sharutc.ui.custom.card.SimpleCard;
+import fr.utc.lo23.sharutc.ui.custom.card.SongCardRight;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.FlowPane;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import fr.utc.lo23.sharutc.ui.custom.card.SongCardRight;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.layout.FlowPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * FXML Controller class
  *
  * @author shima
  */
-public class GroupRightController implements Initializable, SongCardRight.ISongCardRight {
+public class GroupRightController implements Initializable, SongCardRight.ISongCardRight, RightCard.IRightCard {
 
-    
+
     @Inject
     private ManageRightsCommand rightCommand;
     @FXML
     public FlowPane songsContainer;
     @FXML
+    public FlowPane rightContainer;
+    @FXML
+    public ScrollPane rightScrollPane;
+    @FXML
     public Label Group;
     private Category currentCategory;
     private static final Logger log = LoggerFactory.getLogger(GroupRightController.class);
+
+    private SimpleCard mAllSongCard;
+    private RightCard mReadSongCard;
+    private RightCard mListenSongCard;
+    private RightCard mCommentAndNoteSongCard;
+    private RightCard mAllRightsSongCard;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-          for (int i = 0; i < 20; i++) {
+        rightScrollPane.getStyleClass().add("myScrollPaneWithTopBorder");
+        HorizontalScrollHandler scrollHandler = new HorizontalScrollHandler(rightScrollPane);
+
+        for (int i = 0; i < 20; i++) {
             final Music m = new Music();
             m.setTitle("Music " + i);
             m.setArtist("Artist" + i);
-            SongCardRight newCard = new SongCardRight(m,this,false,true,false,true);
+            SongCardRight newCard = new SongCardRight(m, this, false, true, false, true);
             songsContainer.getChildren().add(newCard);
         }
         // TODO
+        drawRightCard();
     }
-    
-    
+
+
     public void setGroupInfo(Category category) {
- 
+
         Group.setText(category.getName());
-        currentCategory=category;
+        currentCategory = category;
     }
-    
-    
-    
-    public void handleCheckBoxListenClicked(Music m,boolean boxstate) {
-        
+
+
+    public void handleCheckBoxListenClicked(Music m, boolean boxstate) {
+
         rightCommand.setCategory(currentCategory);
         rightCommand.setMusic(m);
-        if (boxstate){
-            log.info("Goup:" + Group.getText()+" gives maylisten rights on music :"+m.getTitle());
+        if (boxstate) {
+            log.info("Goup:" + Group.getText() + " gives maylisten rights on music :" + m.getTitle());
             rightCommand.setMayListen(true);
-            }
-        
-        else {
-            log.info("Goup:" + Group.getText()+" removes maylisten rights on music :"+m.getTitle());
+        } else {
+            log.info("Goup:" + Group.getText() + " removes maylisten rights on music :" + m.getTitle());
             rightCommand.setMayListen(false);
-            }
+        }
         rightCommand.execute();
     }
-    public void handleCheckBoxCommentClicked(Music m,boolean boxstate) {
+
+    public void handleCheckBoxCommentClicked(Music m, boolean boxstate) {
         rightCommand.setCategory(currentCategory);
         rightCommand.setMusic(m);
-        if (boxstate){
-            log.info("Goup:" + Group.getText()+" gives Comment rights on music :"+m.getTitle());
+        if (boxstate) {
+            log.info("Goup:" + Group.getText() + " gives Comment rights on music :" + m.getTitle());
             rightCommand.setMayCommentAndScore(true);
-        }
-        else{
-            log.info("Goup:" + Group.getText()+" removes Comment rights on music :"+m.getTitle());
+        } else {
+            log.info("Goup:" + Group.getText() + " removes Comment rights on music :" + m.getTitle());
             rightCommand.setMayCommentAndScore(false);
         }
         rightCommand.execute();
     }
-    public void handleCheckBoxReadClicked(Music m,boolean boxstate) {
+
+    public void handleCheckBoxReadClicked(Music m, boolean boxstate) {
         rightCommand.setCategory(currentCategory);
         rightCommand.setMusic(m);
-        if (boxstate){
-            log.info("Goup:" + Group.getText()+" gives ReadInfo rights on music :"+m.getTitle());
+        if (boxstate) {
+            log.info("Goup:" + Group.getText() + " gives ReadInfo rights on music :" + m.getTitle());
             rightCommand.setMayReadInfo(true);
-        }
-        else{
-            log.info("Goup:" + Group.getText()+" removes ReadInfo rights on music :"+m.getTitle());
+        } else {
+            log.info("Goup:" + Group.getText() + " removes ReadInfo rights on music :" + m.getTitle());
             rightCommand.setMayReadInfo(false);
         }
         rightCommand.execute();
+    }
+
+    /**
+     * display right card
+     */
+    private void drawRightCard() {
+
+        final Label allSongLabel = new Label("Songs");
+        allSongLabel.getStyleClass().add("largeTextGreyBold");
+        mAllSongCard = new SimpleCard("/fr/utc/lo23/sharutc/ui/fxml/simple_card.fxml",
+                180, 101, Pos.CENTER);
+        mAllSongCard.getChildren().add(allSongLabel);
+
+        mReadSongCard = new RightCard("Read", this);
+
+        mListenSongCard = new RightCard("Listen", this);
+
+        mCommentAndNoteSongCard = new RightCard("Note", this);
+
+        mAllRightsSongCard = new RightCard("All Right", this);
+
+        rightContainer.getChildren().addAll(mAllSongCard, mReadSongCard, mListenSongCard, mCommentAndNoteSongCard,
+                mAllRightsSongCard);
+    }
+
+    @Override
+    public void onSongAdded(RightCard card) {
+        //To change body of implemented methods use File | Settings | File Templates.
     }
 }
---
+++ src/fr/utc/lo23/sharutc/ui/custom/card/RightCard.java
@@ -0,0 +1,139 @@
+package fr.utc.lo23.sharutc.ui.custom.card;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.Region;
+
+public class RightCard extends SimpleCard implements EventHandler<Event> {
+
+    @FXML
+    public Label rightName;
+
+    @FXML
+    public Region dropOverlay;
+
+    @FXML
+    public Label dropOverlayLabel;
+
+    private IRightCard mInterface;
+
+    /**
+     * Card displayed to represent a right
+     *
+     * @param rightText text displayed to the user
+     * @param i         interface to get the callback
+     */
+    public RightCard(String rightText, IRightCard i) {
+        super("/fr/utc/lo23/sharutc/ui/fxml/right_card.fxml");
+
+        mInterface = i;
+
+        rightName.setText(rightText);
+
+        setOnDragEntered(this);
+        setOnDragOver(this);
+        setOnDragExited(this);
+        setOnDragDropped(this);
+
+        displayDropOverlay(false);
+    }
+
+    @Override
+    public void handle(Event event) {
+        final Object source = event.getSource();
+        if (event.getEventType() == DragEvent.DRAG_OVER) {
+            onDragOver((DragEvent) event);
+        } else if (event.getEventType() == DragEvent.DRAG_ENTERED) {
+            onDragEntered((DragEvent) event);
+        } else if (event.getEventType() == DragEvent.DRAG_EXITED) {
+            onDragExited((DragEvent) event);
+        } else if (event.getEventType() == DragEvent.DRAG_DROPPED) {
+            onDragDropped((DragEvent) event);
+        }
+    }
+
+    /**
+     * Accept SongCard as Droppable
+     *
+     * @param dragEvent
+     */
+    private void onDragOver(DragEvent dragEvent) {
+        final Object gestureSource = dragEvent.getGestureSource();
+        if (gestureSource instanceof SongCard) {
+            dragEvent.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+        }
+        //don't consume because root uses it to relocate preview
+        //dragEvent.consume();
+    }
+
+    /**
+     * Inform user with a message when an accepted droppable enter
+     *
+     * @param dragEvent
+     */
+    private void onDragEntered(DragEvent dragEvent) {
+        final Object gestureSource = dragEvent.getGestureSource();
+        if (gestureSource instanceof SongCard) {
+            dragEvent.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+            displayDropOverlay(true);
+        }
+        dragEvent.consume();
+    }
+
+    /**
+     * Delete the message when user leave the card
+     *
+     * @param dragEvent
+     */
+    private void onDragExited(DragEvent dragEvent) {
+        final Object gestureSource = dragEvent.getGestureSource();
+        if (gestureSource instanceof SongCard) {
+            displayDropOverlay(false);
+        }
+        dragEvent.consume();
+    }
+
+    /**
+     * retrieve content of the drag
+     *
+     * @param dragEvent
+     */
+    private void onDragDropped(DragEvent dragEvent) {
+        final Dragboard db = dragEvent.getDragboard();
+        boolean success = false;
+        if (db.hasString() && db.getString().equals(PeopleCard.DROP_KEY)) {
+            final SongCard songCard = (SongCard) dragEvent.getGestureSource();
+            mInterface.onSongAdded(this);
+        }
+        dragEvent.setDropCompleted(success);
+        dragEvent.consume();
+    }
+
+    /**
+     * display drop overlay
+     *
+     * @param isShow true set drop overlay Visible, false will hide it
+     */
+    private void displayDropOverlay(boolean isShow) {
+        dropOverlay.setVisible(isShow);
+        dropOverlayLabel.setVisible(isShow);
+    }
+
+    /**
+     * interface for RightCard's callback
+     */
+    public interface IRightCard {
+
+        /**
+         * user added song to this right
+         *
+         * @param card
+         */
+        public void onSongAdded(RightCard card);
+    }
+}
