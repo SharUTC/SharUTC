f0f5066e61f3193719a22f01ab6b695b53aabfb7
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-12-07 11:29:43 +0100

    Implement drag&drop&preview behavior for right

--- src/fr/utc/lo23/sharutc/ui/GroupEditController.java
+++ src/fr/utc/lo23/sharutc/ui/GroupEditController.java
@@ -1,19 +1,17 @@
 package fr.utc.lo23.sharutc.ui;
 
-import fr.utc.lo23.sharutc.model.domain.Music;
-import java.net.URL;
-import java.util.ResourceBundle;
+import fr.utc.lo23.sharutc.model.userdata.Category;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import fr.utc.lo23.sharutc.model.userdata.Category;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import fr.utc.lo23.sharutc.ui.custom.card.SongCardRight;
+
 import java.io.IOException;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.layout.FlowPane;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * FXML Controller class
@@ -22,33 +20,34 @@ import javafx.scene.layout.FlowPane;
  */
 public class GroupEditController implements Initializable {
 
- 
+
     @FXML
     public Label Group;
     @FXML
     TextField NewNameGroup;
-    
+
     private static final Logger log = LoggerFactory.getLogger(GroupEditController.class);
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-          
-        }
-        // TODO
-    
-    
+
+
+    }
+    // TODO
+
+
     public void setGroupInfo(Category category) {
- 
+
         Group.setText(category.getName());
     }
-    
+
     public void handleNewGroupNameEntered(ActionEvent event) throws IOException {
-        log.info(Group.getText()+" as been changed to " +NewNameGroup.getText());
+        log.info(Group.getText() + " as been changed to " + NewNameGroup.getText());
         Group.setText(NewNameGroup.getText());
     }
-    
-  
+
+
 }
