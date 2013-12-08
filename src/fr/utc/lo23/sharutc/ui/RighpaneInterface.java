f249e71c0d85bf0e8348f5f7d85fd647354724b5
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-12-08 15:51:29 +0100

    Delete auto generated comments

--- src/fr/utc/lo23/sharutc/ui/RighpaneInterface.java
+++ src/fr/utc/lo23/sharutc/ui/RighpaneInterface.java
@@ -1,18 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package fr.utc.lo23.sharutc.ui;
 
-/**
- *
- * @author Florian
- */
 public interface RighpaneInterface {
-    
-    
+
     public void onDetach();
-    
 }
