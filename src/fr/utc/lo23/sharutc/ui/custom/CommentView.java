665dd45e0cd2f613f893fef0b1fc48e0f865389e
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-12-09 09:30:14 +0100

    Add some comments for the cutom views.

--- src/fr/utc/lo23/sharutc/ui/custom/CommentView.java
+++ src/fr/utc/lo23/sharutc/ui/custom/CommentView.java
@@ -12,6 +12,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 
+/**
+ * A simple {@link VBox} that shows the author and the content of a comment.
+ *
+ * This class can notify a {@link IComment} when the user interacts with the
+ * view.
+ */
 public class CommentView extends VBox {
 
     @FXML
@@ -28,6 +34,13 @@ public class CommentView extends VBox {
     public Button buttonCancelComment;
     final private Comment mComment;
 
+    /**
+     * Create a {@link CommentView} with no control whatsoever.
+     *
+     * This constructor is usually used for comments that the user didn't make.
+     *
+     * @param comment the {@link Comment} to be shown.
+     */
     public CommentView(final Comment comment) {
 
         mComment = comment;
@@ -47,6 +60,14 @@ public class CommentView extends VBox {
 
     }
 
+    /**
+     * Create a {@link CommentView} with an "edit" and "delete" buttons.
+     *
+     * This constructor is usually used for comments that the user made.
+     *
+     * @param comment the {@link Comment} to be shown.
+     * @param iComment the {@link IComment} to be notified.
+     */
     public CommentView(final Comment comment, final IComment iComment) {
         this(comment);
         final TextArea textAreaEdition = new TextArea();
@@ -82,11 +103,24 @@ public class CommentView extends VBox {
         });
     }
 
+    /**
+     * Confirm the edition of a comment by asking the {@link IComment} to
+     * actually edit the comment.
+     *
+     * @param iComment the {@link IComment} to be notified.
+     * @param textAreaEdition the {@link TextArea} that holds the new comment.
+     */
     private void confirmEdition(final IComment iComment, final TextArea textAreaEdition) {
         final String newCommentText = textAreaEdition.getText();
         iComment.onCommentEditionRequest(mComment, newCommentText);
     }
 
+    /**
+     * Cancel the edition of a comment.
+     *
+     * @param textAreaEdition the {@link  TextArea} that was holding the new
+     * comment.
+     */
     private void cancelEdition(final TextArea textAreaEdition) {
         buttonAcceptComment.setVisible(false);
         buttonCancelComment.setVisible(false);
@@ -95,6 +129,12 @@ public class CommentView extends VBox {
         CommentView.this.getChildren().add(commentContent);
     }
 
+    /**
+     * Start the edition of the comment.
+     *
+     * @param textAreaEdition the {@link TextArea} that will hold the new
+     * comment.
+     */
     private void startEdition(final TextArea textAreaEdition) {
         buttonEditComment.setVisible(false);
         buttonCancelComment.setVisible(true);
@@ -106,6 +146,13 @@ public class CommentView extends VBox {
         CommentView.this.getChildren().add(textAreaEdition);
     }
 
+    /**
+     * Prompt a confirmation box to ask the user if he really wants to delete
+     * the comment.
+     *
+     * @param iComment the {@link IComment} to be notified if the user confirms
+     * the deletion.
+     */
     private void askDeletionConfirmation(final IComment iComment) {
         DialogBoxBuilder.createConfirmBox("Do you really want to delete the comment ?",
                 this.getClass().getResource("/fr/utc/lo23/sharutc/ui/css/modal.css").toExternalForm(),
@@ -120,10 +167,24 @@ public class CommentView extends VBox {
         }).show();
     }
 
+    /**
+     * A Simple interface used by a {@link CommentView}.
+     */
     public interface IComment {
 
+        /**
+         * The {@link IComment} is being asked to edit a {@link Comment}
+         *
+         * @param comment the {@link Comment} to be edited.
+         * @param newCommentText the new content of the comment.
+         */
         public void onCommentEditionRequest(final Comment comment, final String newCommentText);
 
+        /**
+         * The {@link IComment} is being asked to delete a {@link Comment}.
+         *
+         * @param comment the {@link Comment} to be deleted.
+         */
         public void onCommentDeletionRequest(final Comment comment);
     }
 }
--- src/fr/utc/lo23/sharutc/ui/custom/PlayListMusic.java
+++ src/fr/utc/lo23/sharutc/ui/custom/PlayListMusic.java
@@ -2,6 +2,12 @@ package fr.utc.lo23.sharutc.ui.custom;
 
 import fr.utc.lo23.sharutc.model.domain.Music;
 
+/**
+ * A simple model of a piece of music.
+ *
+ * Encapsulate a piece of {@link Music} and a boolean describing its current
+ * playing state.
+ */
 public class PlayListMusic {
 
     public Music music;
@@ -12,10 +18,20 @@ public class PlayListMusic {
         this.played = false;
     }
 
+    /**
+     * Change the current playing state.
+     *
+     * @param b the new playing state.
+     */
     public void setPlaying(boolean b) {
         played = b;
     }
 
+    /**
+     * Get the current playing state.
+     *
+     * @return true if the music is being played, false otherwise.
+     */
     public boolean isPlaying() {
         return played;
     }
