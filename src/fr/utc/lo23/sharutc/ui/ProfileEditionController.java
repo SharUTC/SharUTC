c49a6de8a2ac6f21ddddfbc5dda66342cd96a0b9
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-12-15 17:34:42 +0100

    Add form verification

--- src/fr/utc/lo23/sharutc/ui/ProfileEditionController.java
+++ src/fr/utc/lo23/sharutc/ui/ProfileEditionController.java
@@ -1,19 +1,124 @@
 package fr.utc.lo23.sharutc.ui;
 
+import com.google.inject.Inject;
+import fr.utc.lo23.sharutc.model.AppModel;
+import fr.utc.lo23.sharutc.model.userdata.UserInfo;
+import fr.utc.lo23.sharutc.ui.util.FormUtils;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * An FXML Controller class used to edit the user's profile.
  */
 public class ProfileEditionController implements Initializable {
 
+    private static final Logger log = LoggerFactory
+            .getLogger(ProfileEditionController.class);
+    @FXML
+    public VBox errorContainer;
+    @FXML
+    public TextField textFieldLastName;
+    @FXML
+    public TextField textFieldFirstName;
+    @FXML
+    public TextField textFieldAge;
+    @FXML
+    public PasswordField passwordFieldCurrent;
+    @FXML
+    public PasswordField passwordFieldNew;
+    @FXML
+    public PasswordField passwordFieldConfirmNew;
+    @FXML
+    public Button buttonSaveProfile;
+    @FXML
+    public Button buttonChangePassword;
+    private EventHandler<ActionEvent> mButtonHandler;
+    @Inject
+    AppModel mAppModel;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-    }    
+        errorContainer.getChildren().clear();
+
+        //Listen to ActionEvent on the two buttons
+        mButtonHandler = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent t) {
+                final Object source = t.getSource();
+                if (source.equals(buttonSaveProfile)) {
+                    log.debug("save profile clicked !");
+                    errorContainer.getChildren().clear();
+                    if (isEditFormValid()) {
+                        //TODO edit profile
+                    }
+                } else if (source.equals(buttonChangePassword)) {
+                    log.debug("change password clicked !");
+                    errorContainer.getChildren().clear();
+                    if (isPasswordFormValid()) {
+                        //TODO change password
+                    }
+                }
+            }
+        };
+        buttonChangePassword.setOnAction(mButtonHandler);
+        buttonSaveProfile.setOnAction(mButtonHandler);
+    }
+
+    private boolean isEditFormValid() {
+        final String lastName = textFieldLastName.getText();
+        final String firstName = textFieldFirstName.getText();
+        final String ageStr = textFieldAge.getText();
+        final UserInfo currentUserInfo = mAppModel.getProfile().getUserInfo();
+        boolean isValid = false;
+
+        if (lastName.isEmpty() || firstName.isEmpty() || ageStr.isEmpty()) {
+            showErrorMessage("All fields must be filled!");
+        } else if (!FormUtils.isAgeValid(ageStr)) {
+            showErrorMessage("Age is not valid!");
+        } else {
+            isValid = true;
+        }
+
+        return isValid;
+    }
+
+    private boolean isPasswordFormValid() {
+        final String passwordCurrent = passwordFieldCurrent.getText();
+        final String passwordNew = passwordFieldNew.getText();
+        final String passwordConfirm = passwordFieldConfirmNew.getText();
+        boolean isValid = false;
+
+        if (passwordCurrent.isEmpty() || passwordNew.isEmpty()
+                || passwordConfirm.isEmpty()) {
+            showErrorMessage("All fields must be filled!");
+        } else if (!passwordNew.equals(passwordConfirm)) {
+            showErrorMessage("Passwords don't match!");
+        } else if (!mAppModel.getProfile().getUserInfo().getPassword().equals(UserInfo.sha1(passwordCurrent))) {
+            showErrorMessage("Your current password is wrong!");
+        } else if (passwordCurrent.equals(passwordNew)) {
+            showErrorMessage("You can't change for the same password!");
+        } else {
+            isValid = true;
+        }
+
+        return isValid;
+    }
+
+    private void showErrorMessage(String errorMessage) {
+        errorContainer.getChildren().add(new Label(errorMessage));
+    }
 }
